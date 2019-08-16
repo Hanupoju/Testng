fa7dcb32b310a142f8aaf3ce9838658be713dd60
hanupoju <39627997+hanupoju@users.noreply.github.com>
2019-08-16 17:59:58 +0530

    update NewTest.java
    update NewTest.

--- src/test/java/test/NewTest.java
+++ src/test/java/test/NewTest.java
@@ -10,9 +10,18 @@ public class NewTest {
 	
   @Test
   public void test(){
-	  System.setProperty("webdriver.chrome.driver", "D:\\chromedriver.exe");
-	  WebDriver driver=new ChromeDriver();
-	  driver.get("http://demo.guru99.com/test/guru99home/");  
+// 	  System.setProperty("webdriver.chrome.driver", "D:\\chromedriver.exe");
+	   
+
+        // Add options to Google Chrome. The window-size is important for responsive sites
+
+        ChromeOptions options = new ChromeOptions();
+
+        options.addArguments("headless");
+
+        options.addArguments("window-size=1200x600");
+	  
+	 driver.get("http://demo.guru99.com/test/guru99home/");  
 		String title = driver.getTitle();				 
 		Assert.assertTrue(title.contains("Demo Guru99 Page"));
   }
